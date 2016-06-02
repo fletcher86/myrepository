8bb98cde743c191d07c6ab1248fa4565e631d5b9
Kent Fletcher <kent.fletcher@cox.com>
2016-06-02 03:15:31 -0400

    add documentation

--- siteminder/src/main/groovy/com/its/openpath/module/siteminder/handlers/TakeActionOnStatusThread.groovy
+++ siteminder/src/main/groovy/com/its/openpath/module/siteminder/handlers/TakeActionOnStatusThread.groovy
@@ -3,6 +3,14 @@ package com.its.openpath.module.siteminder.handlers
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * <code>TakeActionOnStatusThread.groovy</code>
+ * <p/>
+ * Take Action on a given status
+ * <p/>
+ * @author Lyle Fletcher
+ * @since Sep 6, 2012
+ */
 public class TakeActionOnStatusThread
 implements Runnable
 {
